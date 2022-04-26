1c5a847f04f244f202fb0784e893941c60efdb35
Ho PC <tjsgh741@gmail.com>
2022-04-26 23:01:37 +0900

    Android project V_team upload

---
+++ app/src/main/java/com/labproject/findme/Sub1_Menu1Activity.java
@@ -0,0 +1,26 @@
+package com.labproject.findme;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Sub1_Menu1Activity extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        return inflater.inflate(R.layout.sub1_menu1, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
